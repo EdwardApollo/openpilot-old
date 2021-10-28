00d7ae3813ab2e768840bae97d66f62b6a371528
Harald Schafer <harald.the.engineer@gmail.com>
2021-10-27 17:33:47 -0700

    chiiiill

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -244,7 +244,7 @@ class LongitudinalMpc():
       self.solver.cost_set(i, 'Zl', Zl)
 
   def set_weights_for_xva_policy(self):
-    W = np.asfortranarray(np.diag([0., 10., 1., 10., 0.0, 1.]))
+    W = np.diag([0., 0.1, .5, 1., 0.0, 1.])
     for i in range(N):
       self.solver.cost_set(i, 'W', W)
     # Setting the slice without the copy make the array not contiguous,
@@ -301,7 +301,7 @@ class LongitudinalMpc():
     self.cruise_max_a = max_a
 
   def update(self, carstate, radarstate, v_cruise, x, v, a):
-    v_ego = self.x0[1]
+    #v_ego = self.x0[1]
     self.yref[:,1] = x
     self.yref[:,2] = v
     self.yref[:,3] = a
