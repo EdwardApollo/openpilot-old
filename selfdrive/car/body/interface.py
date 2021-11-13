830c06d6b1a901d44b38d35f3f3699ed8e55a270
Comma Device <device@comma.ai>
2021-11-12 21:18:27 -0800

    it is like car

--- selfdrive/car/body/interface.py
+++ selfdrive/car/body/interface.py
@@ -6,25 +6,49 @@ from hexdump import hexdump
 
 # sudo chmod 666 /dev/ttyUSB0
 
-ser = serial.Serial('/dev/ttyUSB0', 115200)
-print(ser)
+def open_serial():
+  return serial.Serial('/dev/ttyUSB0', 115200)
 
-def cmd(steer, speed):
+def cmd(ser, steer, speed):
   ret = b"\xcd\xab" + struct.pack("hhH", steer, speed, 0xabcd ^ steer ^ speed)
   ser.write(ret)
 
-def getframe():
+def getframe(ser):
   while 1:
     while ser.read() != b"\xcd":
       continue
     if ser.read() == b"\xab":
       return struct.unpack("hhhhhhHH", ser.read(8*2))
 
+from selfdrive.car.interfaces import CarInterfaceBase
+from cereal import car
+
+class CarInterface(CarInterfaceBase):
+  def __init__(self, CP, CarController, CarState):
+    super().__init__(CP, CarController, CarState)
+    self.ser = open_serial()
+
+  def update(self, c, can_strings):
+    dat = getframe(self.ser)
+    ret = car.CarState.new_message()
+    ret.wheelSpeeds.fl = -dat[2]
+    ret.wheelSpeeds.fr = dat[3]
+    return ret
+
+  def apply(self, c):
+    cmd(self.ser, int(c.actuators.steer), int(c.actuators.accel))
+
 if __name__ == "__main__":
+  ci = CarInterface(car.CarParams.new_message(), None, None)
   while 1:
-    cmd(0, 100)
-    dat = getframe()
-    print(time.time(), dat)
+    cs = ci.update(None, None)
+    print(cs.wheelSpeeds)
+    ret = car.CarControl.new_message()
+    ret.actuators.steer = 0
+    ret.actuators.accel = 100
+    ci.apply(ret)
+
+
 
 
 
