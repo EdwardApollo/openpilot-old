1b2ea6c186451138cb813ca31e8c101fd866589e
Harald Schafer <harald.the.engineer@gmail.com>
2021-11-14 15:17:46 -0800

    super eyes

--- selfdrive/ui/eyes.cc
+++ selfdrive/ui/eyes.cc
@@ -1,27 +1,49 @@
 #include <QApplication>
 #include <QtWidgets>
 
+#include <sys/resource.h>
+
+#include "cereal/messaging/messaging.h"
 #include "selfdrive/ui/qt/qt_window.h"
 #include "selfdrive/ui/qt/util.h"
 #include "selfdrive/ui/qt/widgets/cameraview.h"
 
+class Eyes : public QWidget {
+public:
+  Eyes(QWidget *parent = 0) : sm({"carState"}), QWidget(parent) {
+    QVBoxLayout *layout = new QVBoxLayout(this);
+    layout->setMargin(0);
+    layout->setSpacing(0);
+
+    QLabel *l = new QLabel();
+    QPixmap p("eyes.png");
+    l->setPixmap(p.scaled(QSize(2160, 1080)));
+    layout->addWidget(l);
+
+    QTimer *timer = new QTimer(this);
+    QObject::connect(timer, &QTimer::timeout, [=]() {
+      sm.update(0);
+      // scale volume with speed
+      if (sm.updated("carState")) {
+        if (sm["carState"].getCarState().getStandstill()) {
+          QPixmap p("sleeping.png");
+          l->setPixmap(p.scaled(QSize(2160, 1080)));
+        } else {
+          QPixmap p("moving.png");
+          l->setPixmap(p.scaled(QSize(2160, 1080)));
+        }
+      }
+    });
+  };
+
+private:
+  SubMaster sm;
+};
+
 int main(int argc, char *argv[]) {
-  QSurfaceFormat fmt;
-  fmt.setRenderableType(QSurfaceFormat::OpenGLES);
-  QSurfaceFormat::setDefaultFormat(fmt);
-
+  initApp();
   QApplication a(argc, argv);
-  QWidget w;
-  setMainWindow(&w);
-
-  QVBoxLayout *layout = new QVBoxLayout(&w);
-  layout->setMargin(0);
-  layout->setSpacing(0);
-
-  QLabel *l = new QLabel();
-  QPixmap p("eyes.png");
-  l->setPixmap(p.scaled(QSize(2160, 1080)));
-  layout->addWidget(l);
-
+  Eyes eyes;
+  setMainWindow(&eyes);
   return a.exec();
 }
