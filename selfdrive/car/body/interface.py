26961d67958d938ae93d7dc67760fd9c06b869d0
Comma Device <device@comma.ai>
2021-11-13 23:35:14 -0800

    little better

--- selfdrive/car/body/interface.py
+++ selfdrive/car/body/interface.py
@@ -68,14 +68,11 @@ if __name__ == "__main__":
   dt = 1/dtn
   intgyro = None
 
-  #kp = 4000
-  #kp = 800
   kp = 1000
-  #ki = 1/5
   ki = 0
-  #kd = 1000
-  kd = 300
+  kd = 250
   i = 0
+  d = 0
   i_speed = 0
 
   last_err = 0
@@ -90,7 +87,11 @@ if __name__ == "__main__":
   measured_steer = 0
   desired_speed = 0
   desired_steer = 0
+  time = 0.0
+  torque_a_filt = 0.0
+  torque_b_filt = 0.0
   while 1:
+    time += dt
     sm.update()
 
     try:
@@ -101,16 +102,19 @@ if __name__ == "__main__":
 
 
     alpha = 1.0
-    desired_speed = (1. - alpha)*desired_speed + alpha *(-joy_y/2)
+    desired_speed = (1. - alpha)*desired_speed + alpha *(joy_y/2)
     desired_steer = (1. - alpha)*desired_steer + alpha*joy_x
     kp_speed = 0.001
-    ki_speed = 0.000001
+    ki_speed = 0.00001
     i_speed += ki_speed * (desired_speed - measured_speed)
-    i_speed = np.clip(i_speed, -0.05, 0.05)
+    i_speed = np.clip(i_speed, -0.1, 0.1)
     set_point =  kp_speed * (desired_speed - measured_speed) + i_speed
     try:
       err = (-sm['liveLocationKalman'].orientationNED.value[1]) - set_point
-      d =  -sm['liveLocationKalman'].angularVelocityDevice.value[1]
+      d_new =  -sm['liveLocationKalman'].angularVelocityDevice.value[1]
+      alpha_d = 1.0
+      d = (1. - alpha_d) * d + alpha * d_new
+      d =  np.clip(d, -1., 1.)
     except Exception:
       continue
 
@@ -135,12 +139,26 @@ if __name__ == "__main__":
     kp_steer = 0.2
     diff_torque = kp_steer * (desired_steer - measured_steer)
 
-    ret.actuators.steer = speed + diff_torque
-    ret.actuators.accel = speed - diff_torque
+    #ret.actuators.steer = speed + diff_torque
+    #ret.actuators.accel = speed - diff_torque
+    #A = 200
+    #print(time)
+    #ret.actuators.steer = float(A * np.sin(50*time)) # speed + diff_torque
+    #ret.actuators.accel = float(A * np.sin(50*time)) # speed - diff_torque
+    torque_a = speed + diff_torque
+    torque_b = speed - diff_torque
+    alpha_torque = .3
+    torque_a_filt = (1. - alpha_torque) * torque_a_filt + alpha_torque * torque_a
+    torque_b_filt = (1. - alpha_torque) * torque_b_filt + alpha_torque * torque_b
+    ret.actuators.steer = torque_a_filt
+    ret.actuators.accel = torque_b_filt
     #ret.actuators.steer = joy_x*2
     #ret.actuators.accel = 5
     #ret.actuators.accel =
-    print("%7.2f %7.2f %7.2f %7.2f" % (err*180/3.1415, err, acc_err, d), cs.wheelSpeeds, ret.actuators.accel, joy_x, joy_y)
+    #print("%7.2f %7.2f %7.2f %7.2f" % (err*180/3.1415, err, acc_err, d), cs.wheelSpeeds, ret.actuators.accel, joy_x, joy_y)
+    print(f'joy forward : {joy_y}, joy steer: {joy_x}')
+    print(f'Set point : {set_point}')
+    print(f'speeds : {cs.wheelSpeeds}')
     acc_err += cs.wheelSpeeds.fl + cs.wheelSpeeds.fr
     msgs = ci.apply(ret)
     pm.send('sendcan', can_list_to_can_capnp(msgs, msgtype='sendcan'))
