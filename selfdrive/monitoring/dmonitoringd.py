5352cb73a9b3d8cd4a3e57897210a0eadd0054c1
ZwX1616 <zwx1616@gmail.com>
2022-01-05 14:14:32 -0800

    v0

--- selfdrive/monitoring/dmonitoringd.py
+++ selfdrive/monitoring/dmonitoringd.py
@@ -39,7 +39,7 @@ def dmonitoringd_thread(sm=None, pm=None):
                         sm['carState'].steeringPressed or \
                         sm['carState'].gasPressed
       if driver_engaged:
-        driver_status.update(Events(), True, sm['controlsState'].enabled, sm['carState'].standstill)
+        driver_status.update(Events(), True, sm['controlsState'].enabled, False)
       v_cruise_last = v_cruise
 
     if sm.updated['modelV2']:
@@ -55,7 +55,7 @@ def dmonitoringd_thread(sm=None, pm=None):
       events.add(car.CarEvent.EventName.tooDistracted)
 
     # Update events from driver state
-    driver_status.update(events, driver_engaged, sm['controlsState'].enabled, sm['carState'].standstill)
+    driver_status.update(events, driver_engaged, sm['controlsState'].enabled, False)
 
     # build driverMonitoringState packet
     dat = messaging.new_message('driverMonitoringState')
