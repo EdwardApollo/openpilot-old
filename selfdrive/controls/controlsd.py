f23d817e4107076ddb414d21148e1a7fe7815806
Alex Wang <36569033@qq.com>
2022-02-03 22:43:23 +0800

    Send query fw version request every 0.1 seconds

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -28,6 +28,112 @@ from selfdrive.locationd.calibrationd import Calibration
 from selfdrive.hardware import HARDWARE, TICI, EON
 from selfdrive.manager.process_config import managed_processes
 
+import time
+from collections import defaultdict
+from functools import partial
+from typing import Optional
+
+from panda.python.uds import CanClient, IsoTpMessage, FUNCTIONAL_ADDRS, get_rx_addr_for_tx_addr
+import panda.python.uds as uds
+
+UDS_VERSION_REQUEST = bytes([uds.SERVICE_TYPE.READ_DATA_BY_IDENTIFIER]) + \
+  p16(uds.DATA_IDENTIFIER_TYPE.APPLICATION_SOFTWARE_IDENTIFICATION)
+UDS_VERSION_RESPONSE = bytes([uds.SERVICE_TYPE.READ_DATA_BY_IDENTIFIER + 0x40]) + \
+  p16(uds.DATA_IDENTIFIER_TYPE.APPLICATION_SOFTWARE_IDENTIFICATION)
+
+DEFAULT_RX_OFFSET = 0x8
+
+def p16(val):
+  return struct.pack("!H", val)
+
+class IsoTpParallelQuery:
+  def __init__(self, sendcan, logcan, addrs, request, response, response_offset=0x8, functional_addr=False, debug=False):
+    self.sendcan = sendcan
+    self.logcan = logcan
+    # On OBD only
+    self.bus = 1
+    self.request = request
+    self.response = response
+    self.debug = debug
+    self.functional_addr = functional_addr
+
+    self.real_addrs = []
+    for a in addrs:
+      if isinstance(a, tuple):
+        self.real_addrs.append(a)
+      else:
+        self.real_addrs.append((a, None))
+
+    self.msg_addrs = {tx_addr: get_rx_addr_for_tx_addr(tx_addr[0], rx_offset=response_offset) for tx_addr in self.real_addrs}
+    self.msg_buffer = defaultdict(list)
+
+  def rx(self):
+    """Drain can socket and sort messages into buffers based on address"""
+    can_packets = messaging.drain_sock(self.logcan, wait_for_one=True)
+
+    for packet in can_packets:
+      for msg in packet.can:
+        if msg.src == self.bus:
+          if self.functional_addr:
+            if (0x7E8 <= msg.address <= 0x7EF) or (0x18DAF100 <= msg.address <= 0x18DAF1FF):
+              fn_addr = next(a for a in FUNCTIONAL_ADDRS if msg.address - a <= 32)
+              self.msg_buffer[fn_addr].append((msg.address, msg.busTime, msg.dat, msg.src))
+          elif msg.address in self.msg_addrs.values():
+            self.msg_buffer[msg.address].append((msg.address, msg.busTime, msg.dat, msg.src))
+
+  def _can_tx(self, tx_addr, dat, bus):
+    """Helper function to send single message"""
+    msg = [tx_addr, 0, dat, bus]
+    self.sendcan.send(can_list_to_can_capnp([msg], msgtype='sendcan'))
+
+  def _can_rx(self, addr, sub_addr=None):
+    """Helper function to retrieve message with specified address and subadress from buffer"""
+    keep_msgs = []
+
+    if sub_addr is None:
+      msgs = self.msg_buffer[addr]
+    else:
+      # Filter based on subadress
+      msgs = []
+      for m in self.msg_buffer[addr]:
+        first_byte = m[2][0]
+        if first_byte == sub_addr:
+          msgs.append(m)
+        else:
+          keep_msgs.append(m)
+
+    self.msg_buffer[addr] = keep_msgs
+    return msgs
+
+  def _drain_rx(self):
+    messaging.drain_sock(self.logcan)
+    self.msg_buffer = defaultdict(list)
+
+  def send_request(self):
+    # Create message objects
+    msgs = {}
+    request_counter = {}
+    request_done = {}
+    for tx_addr, rx_addr in self.msg_addrs.items():
+      # rx_addr not set when using functional tx addr
+      id_addr = rx_addr or tx_addr[0]
+      sub_addr = tx_addr[1]
+
+      can_client = CanClient(self._can_tx, partial(self._can_rx, id_addr, sub_addr=sub_addr), tx_addr[0], rx_addr,
+                             self.bus, sub_addr=sub_addr, debug=self.debug)
+
+      max_len = 8 if sub_addr is None else 7
+
+      msg = IsoTpMessage(can_client, timeout=0, max_len=max_len, debug=self.debug)
+      msg.send(self.request[0])
+
+      msgs[tx_addr] = msg
+      request_counter[tx_addr] = 0
+      request_done[tx_addr] = False
+    results = {}
+    return results
+
+  
 SOFT_DISABLE_TIME = 3  # seconds
 LDW_MIN_SPEED = 31 * CV.MPH_TO_MS
 LANE_DEPARTURE_THRESHOLD = 0.1
@@ -631,6 +737,9 @@ class Controls:
       self.last_actuators, can_sends = self.CI.apply(CC)
       self.pm.send('sendcan', can_list_to_can_capnp(can_sends, msgtype='sendcan', valid=CS.canValid))
       CC.actuatorsOutput = self.last_actuators
+      if (self.sm.frame % int(0.2 / DT_CTRL) == 0):
+        uds_query = IsoTpParallelQuery(self.pm.sock['sendcan'], self.can_sock,  [(0x18da30f1, None)], [UDS_VERSION_REQUEST], [UDS_VERSION_RESPONSE], DEFAULT_RX_OFFSET, debug=False)
+        uds_query.send_request()
 
     force_decel = (self.sm['driverMonitoringState'].awarenessStatus < 0.) or \
                   (self.state == State.softDisabling)
