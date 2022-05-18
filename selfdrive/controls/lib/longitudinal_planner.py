1a3e93ab53598a8b883901a79c4768e0cf5c4a4d
Casey Francis <>
2022-05-17 23:08:58 -0700

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -17,7 +17,7 @@ from selfdrive.swaglog import cloudlog
 LON_MPC_STEP = 0.2  # first step is 0.2s
 AWARENESS_DECEL = -0.2  # car smoothly decel at .2m/s^2 when user is distracted
 A_CRUISE_MIN = -1.2
-A_CRUISE_MAX_VALS = [2.5, 3.0, 2.5, 1.5, 1.2]  # Sets the limits of the planner accel, PID may exceed
+A_CRUISE_MAX_VALS = [1.4, 1.3, 1.2, 0.8, 0.5]  # Sets the limits of the planner accel, PID may exceed
 A_CRUISE_MAX_BP = [0., 5., 10., 20., 55.]
 
 # Lookup table for turns
